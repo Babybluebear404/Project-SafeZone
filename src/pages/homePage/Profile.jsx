c472963c63f37e5db3933f39d029e0b394d1a5e4
Chaloamwong Srithai <asdqaz_155@hotmail.com>
2025-03-19 17:33:10 +0700

    updateTabicon
    เพิ่มรูปใน icon

--- src/pages/homePage/Profile.jsx
+++ src/pages/homePage/Profile.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { IoIosLogOut } from "react-icons/io";
 import '../../style/Profile.css';
 import { useCookies } from "react-cookie";
+import imageTemplates from '../../components/imageTemplates';
 
 const Profile = ( userService ) => {
   const navigate = useNavigate();
@@ -99,19 +100,6 @@ const Profile = ( userService ) => {
       console.error('Failed to save user data:', error);
     }
   };
-
-  // ลิสต์รูปภาพ
-  const imageTemplates = [
-    {id: 0,src : "/assets/0.png",name: "Banana Cat"},
-    {id: 1,src : "/assets/1.png",name: "Banana Cat"},
-    {id: 2,src : "/assets/2.png",name: "Bread Cat"},
-    {id: 3,src : "/assets/3.png",name: "Cake Cat"},
-    {id: 4,src : "/assets/4.png",name: "Croissant Cat"},
-    {id: 5,src : "/assets/5.png",name: "Hot Milk Cat"},
-    {id: 6,src : "/assets/6.png",name: "Milk Tea Cat"},
-    {id: 7,src : "/assets/7.png",name: "Pudding Cat"},
-    {id: 8,src : "/assets/8.png",name: "Berger Cat"},
-  ];
 
   const togglePopup = () => {
     setIsPopupOpen(!isPopupOpen);
