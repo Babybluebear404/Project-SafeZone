e2a6966cc2c538fc067fdecd9184b4963544269c
whysoakemiii <sujira.jamdee@gmail.com>
2025-06-14 13:21:14 +0700

    update port

--- database/server.js
+++ database/server.js
@@ -57,7 +57,7 @@ app.get('/', (req, res) => {
 });
 
 // เริ่มต้นเซิร์ฟเวอร์
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server running at http://localhost:${port}`);
 });
 
